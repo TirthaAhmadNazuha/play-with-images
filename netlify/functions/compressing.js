701bfc3fd9160d723f0737e48d9f9e7ddf92b115
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2023-07-25 13:44:16 +0700

    developing netlify functions

--- netlify/functions/compressing.js
+++ netlify/functions/compressing.js
@@ -3,24 +3,15 @@ exports.handler = async function (event, context) {
     const body = JSON.parse(event.body);
     const base64Image = body.image; // Assuming you send the image data as "image" in the request body
 
-    const [imagemin, imageminJpegtran] = await Promise.all([
-      import('imagemin'),
-      import('imagemin-jpegtran'),
-    ]);
+    const sharp = (await import('sharp')).default;
 
     // Decode Base64 image data to a Buffer
     const imageBuffer = Buffer.from(base64Image, 'base64');
 
-    // Compress JPEG image
-    const compressedBuffer = await imagemin.default.buffer(imageBuffer, {
-      plugins: [
-        imageminJpegtran.default({
-          progressive: true,
-          arithmetic: false,
-          optimizationLevel: 2,
-        }),
-      ],
-    });
+    // Compress JPEG image using sharp
+    const compressedBuffer = await sharp(imageBuffer)
+      .jpeg({ quality: 80, progressive: true }) // Adjust quality as needed
+      .toBuffer();
 
     // Base64 encode the compressed image
     const compressedBase64 = compressedBuffer.toString('base64');
